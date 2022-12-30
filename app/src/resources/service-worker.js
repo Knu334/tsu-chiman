a9bdf3a165e8fbcd092b66c144520a636038ec67
Knu334 <who.is.fuu@gmail.com>
2022-12-31 04:34:05 +0900

    ServiceWorkerの実装見直し

--- app/src/resources/service-worker.js
+++ app/src/resources/service-worker.js
@@ -1,18 +1,24 @@
 self.addEventListener('install', (event) => {
     console.log('【SW】Install : Service Worker のインストールが開始された', event);
+    event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('push', (event) => {
     console.log('【SW】Push : メッセージを受信した', event, event.data.json());
-    self.registration.showNotification(event.data.json().title, {
+    return event.waitUntil(self.registration.showNotification(event.data.json().title, {
         body: event.data.json().body,
         icon: event.data.json().icon,
         data: event.data.json().data,
         requireInteraction: false
-    });
+    }));
 });
 
 self.addEventListener('notificationclick', (event) => {
+    event.preventDefault();
     event.notification.close();
-    clients.openWindow(event.notification.data);
+    return event.waitUntil(clients.openWindow(event.notification.data));
 });
