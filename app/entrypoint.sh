547159a1b9843f12238266036cccc0862764467c
Knu334 <who.is.fuu@gmail.com>
2023-02-06 02:43:38 +0900

    Dockerコンテナで動作するように修正

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-if [ ! -d ./prisma/migrate ]; then
-    npx prisma migrate dev --name init
-fi
-npx ts-node -r tsconfig-paths/register server.ts
+npx prisma migrate dev --name subscription
+npx ts-node -r tsconfig-paths/register src/server.ts
